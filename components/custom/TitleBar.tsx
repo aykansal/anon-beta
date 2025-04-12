78a2f1c50fa804853c6bcc6b4697631115675c9e
Ayush <ayushkansal303@gmail.com>
2025-04-12 05:40:35 +0530

    fixed build

--- components/custom/TitleBar.tsx
+++ components/custom/TitleBar.tsx
@@ -19,7 +19,6 @@ import {
   DialogHeader,
   DialogTitle,
   DialogFooter,
-  DialogDescription,
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { useGitHub } from '@/context/GitHubContext';
